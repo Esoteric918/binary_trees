a839fcd5657c2f974f0b2080bb4bb0dd4cf0cebb
Esoteric918 <3411@holbertonschool.com>
2022-04-04 13:51:31 -0500

    betty fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,21 +3,21 @@
  * binary_tree_insert_left - inserts new node to left child
  * @parent: parent node
  * @value: value stored in the new node
- * Return NULL if not valid or new node
+ * Return: NULL if not valid or new node
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-	if(!parent)
+	if (!parent)
 		return (NULL);
 
 	node = binary_tree_node(parent, value);
 	if (!node)
 		return (NULL);
 
-	if(parent->left)
+	if (parent->left)
 		parent->left->parent = node;
 
 	node->n = value;
