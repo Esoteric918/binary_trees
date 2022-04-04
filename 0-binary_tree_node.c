d957ece7d853b134cbf5836e75a76a5d5555004b
Esoteric918 <3411@holbertonschool.com>
2022-04-04 12:05:11 -0500

    betty fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,21 +5,19 @@
  *
  * @parent: parent node
  * @value: value to be put in node
- * @return binary_tree_t*
+ * Return: binary tree with values
  */
-
-
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-    if (!node)
-        return (NULL);
+	if (!node)
+		return (NULL);
 
-    node->n = value;
-    node->parent = parent;
-    node->right = NULL;
-    node->left = NULL;
-    return (node);
+	node->n = value;
+	node->parent = parent;
+	node->right = NULL;
+	node->left = NULL;
+	return (node);
 }
