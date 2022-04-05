04698c5ee3ebfb69208c800e054f1f018b81124b
Esoteric918 <3411@holbertonschool.com>
2022-04-05 11:18:28 -0500

    task 11 refactor tres.style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,15 +1,13 @@
 #include "binary_trees.h"
 /**
  * binary_tree_size - measures the size of a binary tree
- *
+ *@tree: tree to be measured
+ *Return: added size of nodes in tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t size = 1;
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    size += binary_tree_size(tree->left);
-    size += binary_tree_size(tree->right);
-    return (size);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
