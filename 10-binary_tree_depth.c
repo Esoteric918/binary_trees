e805c10de0efbde59190f047a247ea2adb14a024
Esoteric918 <3411@holbertonschool.com>
2022-04-05 11:01:16 -0500

    task 10 betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,6 +9,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t nDepth = 0;
 
+	if (!tree)
+		return (0);
+
 	for (; tree->parent; tree = tree->parent, ++nDepth)
 	;
 	return (nDepth);
