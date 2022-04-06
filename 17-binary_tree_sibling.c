887258a4ce12ec3751965d17eac71530103a7fe9
Esoteric918 <3411@holbertonschool.com>
2022-04-06 12:38:28 -0500

    refactor i was way over thinking

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,40 +8,9 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	int level = 0;
-
-	if (node == NULL || node->parent == NULL)
+	if (!node || !node->parent)
 		return (NULL);
-
-	while (node->parent->right == node ||
-			(node->parent->right == NULL &&
-				node->parent->left == node))
-	{
-		if (node->parent == NULL
-			|| node->parent->parent == NULL)
-			return (NULL);
-		node = node->parent;
-		level--;
-	}
-	node = node->parent->right;
-
-	if (node == NULL)
-		return (NULL);
-	while (level < 0)
-	{
-		/* Iterate through subtree */
-		if (node->left != NULL)
-			node = node->left;
-		else if (node->right != NULL)
-			node = node->right;
-		else
-			/* if no child are there, we cannot have right sibling in this path*/
-			break;
-		level++;
-	}
-	/* reset */
-	if (level == 0)
-		return (node);
-
-	return (binary_tree_sibling(node));
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
