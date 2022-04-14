363f46c17f455c7926a22ac170a9aa2c778dd7a5
Esoteric918 <3411@holbertonschool.com>
2022-04-14 10:49:27 -0500

    task 1 betty good

---
+++ 111-bst_insert.c
@@ -0,0 +1,47 @@
+
+#include "binary_trees.h"
+/**
+ * bst_insert - inserts new node to BTS
+ * @tree: tree that new node is to be added
+ * @value: value stored in node
+ * Return: bst_t*
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *curr, *new;
+
+	if (tree != NULL)
+	{
+		curr = *tree;
+
+		if (curr == NULL)
+		{
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
+
+		if (value < curr->n) /* insert in left subtree */
+		{
+			if (curr->left)
+				return (bst_insert(&curr->left, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->left = new);
+		}
+		if (value > curr->n) /* insert in right subtree */
+		{
+			if (curr->right)
+				return (bst_insert(&curr->right, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->right = new);
+		}
+	}
+	return (NULL);
+}
