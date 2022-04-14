79ac4fa8eade09712700f7bc72f051269418c4b2
Esoteric918 <3411@holbertonschool.com>
2022-04-14 09:27:46 -0500

    task 0 add helper func betty good

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,22 +1,48 @@
 #include "binary_trees.h"
 /**
- * @brief
- *
+ * is_it_bst - recursive check for BST
+ * @node: root of tree being checks
+ * @min: minimum value of node
+ * @max: maximum value of node
+ * Return: true if BST valid else false
  */
 
-int binary_tree_is_bst(const binary_tree_t *tree) {
-    if (!tree)
-        return (0);
-        /* false if left is > than node */
-    if (tree->left && tree->left->n >= tree->n)
-        return (0);
-        /* false if left is < than node */
-    if (tree->right && tree->right->n <= tree->n)
-        return (0);
-     /* false if, recursively, the left or right is not a BST */
-    if (!binary_tree_is_bst(tree->left) || !binary_tree_is_bst(tree->right))
-        return 0;
 
-    /* passing all that, it's a BST */
-    return 1;
+bool is_it_bst(const binary_tree_t *node, int min, int max)
+{
+	if (!node)
+		return (true);
+
+	/**
+	 * false if left node violates the min/max constraint
+	 */
+	if (node->left  && (node->left->n >= node->n || node->left->n <= min))
+		return (false);
+
+	/**
+	 * false if right node violates the min/max constraint
+	*/
+	if (node->right  && (node->right->n <= node->n || node->right->n >= max))
+		return (false);
+
+	/**
+	 * otherwise check the subtrees recursively,
+		tightening the min or max constraint
+	*/
+	return (is_it_bst(node->left, min, node->n) &&
+			is_it_bst(node->right, node->n, max));
+}
+
+/**
+ * binary_tree_is_bst- checks if tree is valid BST
+ *
+ * @tree: tree to be checked
+ * Return: 1 if BTS valid else 0
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (is_it_bst(tree, INT_MIN, INT_MAX));
 }
